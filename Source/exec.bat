2cc4e19287bc5efc2de811d1ec4c6bd9f3bf008f
zorkano <seif.mahmoud.mostafa.273@gmail.com>
2024-11-12 11:01:50 +0200

    Organised the repo

---
+++ Source/exec.bat
@@ -0,0 +1,5 @@
+gcc ./code/main.c ./code/school.c ./code/display.c -o ../Release/project
+pause
+cd ../Release/
+project.exe
+pause
--- exec.bat
+++
@@ -1,4 +0,0 @@
-gcc main.c school.c display.c -o project
-pause
-Project.exe
-pause
